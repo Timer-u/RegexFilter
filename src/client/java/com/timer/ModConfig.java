fa838ad76a5f18e96ba0ac7c2400a6d9bb775c44
Timer <145187245+Timer-keeper@users.noreply.github.com>
2025-03-15 12:03:29 +0800

    fix: add missing import statement in

--- src/client/java/com/timer/ModConfig.java
+++ src/client/java/com/timer/ModConfig.java
@@ -2,6 +2,7 @@ package com.timer;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
